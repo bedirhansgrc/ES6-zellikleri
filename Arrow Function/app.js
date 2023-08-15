95d87a972a47ebaf446012ce676c3aa7696b9912
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-08-15 15:12:47 +0300

    created Arrow Function

---
+++ Arrow Function/app.js
@@ -0,0 +1,24 @@
+// const merhaba = function(){
+//     console.log("Merhaba")
+
+// }
+
+// merhaba()
+
+//Arrow Function
+
+// const merhaba = () => {
+//     console.log("Merhaba");
+// }
+
+// merhaba();
+
+// const merhaba = (firstname) => {
+//     console.log("Merhaba", firstname);
+// }
+
+// merhaba("Murat")
+
+const cube = x => x* x* x;
+
+console.log(cube(4));
